40093f54a5ab1caa17aa91b8c1e23f7524b86b06
joy <joyceline.marealle@peopleshores.com>
2025-03-11 20:25:23 -0400

    Pattern 9 Two heaps DFS : 1 median of streams of number

---
+++ PATTERNS/7 Breadth First Search (BFS) Tree pattern/REVIEW/1.py
@@ -0,0 +1,53 @@
+from collections import deque
+class Node :
+    def __init__(self, value, left = None, right =None):
+        self.left = left
+        self.right = right
+        self.value = value
+
+def traverse(root):
+    result = []
+    #edge case
+    if root is None:
+        return []
+
+    queue = deque()
+    #push root to queue
+    queue.append(root)
+
+    #loop as long queue is not empty
+    while queue:
+    #for each level iterate safe the levelSize
+    levelSize = len(queue)
+    currentLevel = [] #empty array to hold nodes of a level
+
+    #remove levelSize nodes from queue and push to array
+    for _ in range(levelSize):
+        currentNode = queue.popleft()
+
+        #add currentNode value to array
+        currentLevel.append(currentNode.value)
+        #when queue empty insert children in queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+        result.append(currentLevel)
+    return result
+
+def main():
+    root = Node(12)
+    root.left = Node(7)
+    root.right = Node(1)
+    root.left.left = Node(9)
+    root.right.left = Node(10)
+    root.right.right = Node(5)
+
+main ()
+
+#push the root to a queu
+#loop through queue as long as it has elements
+    #3 for each iteration determine size of queue + have an empty array
+    #4 remove /pop levelSize nodes from queue and push value to  add it to array --. reprents current level
+    #after removing each node from queue then add children to the queue
+    #if queue not empty repeat 3-5
---
+++ PATTERNS/TWO HEAPS/1 Find median of a number of stream.py
@@ -0,0 +1,53 @@
+from heapq import *
+
+class MedianOfAStream:
+
+    maxHeap = [] #store first half numbers. We will use negative to simulate max-heap
+    minHeap = [] #store second half numbers
+
+    def insert_num(self, num):
+       #1 insert either max or min heap
+        #if num <= to max element in maxheap then insert to maxheap or else insert to minHeap
+        #use negative to show left side of numbers
+        #the top element of maxHeap will always be the max
+        #check if heap is empty and is max element /top >= num
+        if not self.maxHeap or -self.maxHeap[0] >= num:
+            heappush(self.maxHeap, -num)
+        else:
+            heappush(self.minHeap, num)
+
+        #2 Balance elemnt if odd then decided to have max heap has more element than min heap
+        #more element in max heap
+        if len(self.maxHeap) > len(self.minHeap)+1:
+            heappush(self.minHeap, -heappop(self.maxHeap))
+
+        elif len(self.maxHeap) < len(self.minHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap))
+    def find_median(self):
+        return 0.0
+
+
+def main():
+    median_of_a_stream = MedianOfAStream()
+    median_of_a_stream.insert_num(3)
+    print("The median is:" + str(median_of_a_stream.find_median()))
+
+    median_of_a_stream.insert_num(1)
+    print("The median is:" + str(median_of_a_stream.find_median()))
+
+    median_of_a_stream.insert_num(4)
+    print("The median is:" + str(median_of_a_stream.find_median()))
+main()
+
+#Process
+#1 Store values < than top element in Max heap. This is heap containing left side of X median.
+# balance elements in heap if odd  decided that Max heap has more elements
+#2 Store values > than top element in Max heap to Min heap. This heap contains right side of x median
+#3 insert a number into heap which takes order big O(log N)
+#4 Find average if even take average of top element from both heaps. if odd median is top element of hea
+
+
+
+
+
+
