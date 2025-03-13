a881782fcbb2ad2eba1270b3c4d4448727987d25
joy <joyceline.marealle@peopleshores.com>
2025-03-13 08:24:38 -0400

    Pattern 4 Merge intervals : 1.1 tbc review

---
+++ PATTERNS/4 Merging Intervals/Review/1.1.py
@@ -0,0 +1,14 @@
+from __future__ import *
+
+class Interval:
+    def __init__(self,start,end):
+        self.start = start
+        self.end = end
+
+    def print_interval(self):
+        print ("[" + str(self.start) + " " + str(self.end) + " ]")
+
+
+
+
+#
